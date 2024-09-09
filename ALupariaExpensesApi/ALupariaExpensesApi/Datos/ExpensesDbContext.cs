02235af5820cc048c231c82674f1bc0997969973
113973_LUPARIA_AGUSTIN <142342515+LupAgustin@users.noreply.github.com>
2024-09-09 13:46:37 -0300

    Innitial Commit

---
+++ ALupariaExpensesApi/ALupariaExpensesApi/Datos/ExpensesDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ALupariaExpensesApi.Datos
+{
+    public class ExpensesDbContext: DbContext
+    {
+        public ExpensesDbContext(DbContextOptions<ExpensesDbContext> options):base(options) { }
+    }
+}
